51878740edf415ae9057e272724c51e966b9b327
Nathan Rhead Cox <nrc4@hotmail.com>
2020-09-17 21:57:24 -0700

    JS updates.

---
+++ add.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var yeahFord = confirm("You ready to be Forded?");
+
+if (yeahFord == !true) {
+    alert("Chicken. Try again.")
+    location.reload()
+} else {
+    alert("Heck yeah, you are!")
+}
+
+
+
+// if (readyFord = "Yes" || "yes" || "Yeehaw" || "Yessir" || "Yeah, baby" || "You know it") {
+//     document.write("Rad!")
+}
